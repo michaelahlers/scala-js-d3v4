b5f3a9fccf50ee619ec57b400ac4f46ce2e8fd05
Michael Ahlers <michael@ahlers.consulting>
2021-02-28 07:42:23 -0500

    Add support for registering event listeners to transition

--- src/main/scala/Transition.scala
+++ src/main/scala/Transition.scala
@@ -1,3 +1,4 @@
+import d3v4.{Group, Index}
 import org.scalajs.dom
 
 import scala.scalajs.js
@@ -23,6 +24,16 @@ object d3transition extends js.Object {
   @js.native
   trait Transition[Datum] extends js.Object {
 
+    type ValueFunction0[Return] = js.Function0[Return]
+    type ValueFunction1[Return] = js.Function1[Datum, Return]
+    type ValueFunction2[Return] = js.Function2[Datum, Index, Return]
+    //type ValueFunction3[Return] = js.Function3[Datum, Index, Group, Return]
+
+    type ListenerFunction0 = ValueFunction0[Unit]
+    type ListenerFunction1 = ValueFunction1[Unit]
+    type ListenerFunction2 = ValueFunction2[Unit]
+    //type ListenerFunction3 = ValueFunction3[Unit]
+
     /** @see [[https://github.com/d3/d3-transition#transition_delay]] */
     def delay(value: Double): Transition[Datum] = js.native
 
@@ -31,6 +42,15 @@ object d3transition extends js.Object {
 
     /** @see [[https://github.com/d3/d3-transition#transition_empty]] */
     def empty():Boolean = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_on]] */
+    def on(typenames: String, listener: ListenerFunction0): this.type = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_on]] */
+    def on(typenames: String, listener: ListenerFunction1): this.type = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_on]] */
+    def on(typenames: String, listener: ListenerFunction2): this.type = js.native
 
     /** @see [[https://github.com/d3/d3-transition#transition_nodes]] */
     def nodes(): js.Array[dom.EventTarget] = js.native
